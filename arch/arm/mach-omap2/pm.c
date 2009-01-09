efbe51b9fbaeb3c75c657927541bd4347e7e050b
Tero Kristo <tero.kristo@nokia.com>
2009-01-09 17:49:01 +0200

    OMAP3: PM: Fixed VDD2 control to work from both sysfs and SRF API
    SRF API for setting throughput requirement attempted to use throughput values
    as parameter to set_opp() and sysfs was attempting to use OPP levels (1..3.)
    
    Signed-off-by: Tero Kristo <tero.kristo@nokia.com>
    Signed-off-by: Sanjeev Premi <premi@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -62,7 +62,6 @@ static struct kobj_attribute voltage_off_while_idle_attr =
 	__ATTR(voltage_off_while_idle, 0644, idle_show, idle_store);
 
 #ifdef CONFIG_OMAP_PM_SRF
-static struct device dummy_sysfs_dev;
 static ssize_t vdd_opp_show(struct kobject *, struct kobj_attribute *, char *);
 static ssize_t vdd_opp_store(struct kobject *k, struct kobj_attribute *,
 			  const char *buf, size_t n);
@@ -137,7 +136,6 @@ static ssize_t vdd_opp_store(struct kobject *kobj, struct kobj_attribute *attr,
 			  const char *buf, size_t n)
 {
 	unsigned short value;
-	unsigned long bus_tput;
 
 	if (sscanf(buf, "%hu", &value) != 1)
 		return -EINVAL;
@@ -147,15 +145,13 @@ static ssize_t vdd_opp_store(struct kobject *kobj, struct kobj_attribute *attr,
 			printk(KERN_ERR "vdd_opp_store: Invalid value\n");
 			return -EINVAL;
 		}
-		resource_request("vdd1_opp", &dummy_sysfs_dev, value);
+		set_opp_level(VDD1_OPP, value);
 	} else if (attr == &vdd2_opp_attr) {
 		if (value < 2 || value > 3) {
 			printk(KERN_ERR "vdd_opp_store: Invalid value\n");
 			return -EINVAL;
 		}
-		/* Convert OPP's requested to appr. bus throughtput in KiB/s */
-		bus_tput = ((l3_opps + value)->rate/1000) * 4;
-		resource_request("vdd2_opp", &dummy_sysfs_dev, bus_tput);
+		set_opp_level(VDD2_OPP, value);
 	} else {
 		return -EINVAL;
 	}
