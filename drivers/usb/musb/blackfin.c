987940ca9bee7df139f57e9beb450a51e7c3e75e
Ravi B <ravibabu@ti.com>
2010-12-06 18:39:22 +0530

    usb: nop: support for multi-nop tranceiver
    Modified the nop driver to support multi nop transceiver.
    Changes:
    	- Global xceiv to xceiv[2]
    	- nop transceiver regiser/deregiser based on musb->id
    	- Changes to other glue layers
    
    Signed-off-by: Ravi B <ravibabu@ti.com>
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- drivers/usb/musb/blackfin.c
+++ drivers/usb/musb/blackfin.c
@@ -387,8 +387,8 @@ static int bfin_musb_init(struct musb *musb)
 	}
 	gpio_direction_output(musb->config->gpio_vrsel, 0);
 
-	usb_nop_xceiv_register();
-	musb->xceiv = otg_get_transceiver();
+	usb_nop_xceiv_register(musb->id);
+	musb->xceiv = otg_get_transceiver(musb->id);
 	if (!musb->xceiv) {
 		gpio_free(musb->config->gpio_vrsel);
 		return -ENODEV;
@@ -413,7 +413,7 @@ static int bfin_musb_exit(struct musb *musb)
 	gpio_free(musb->config->gpio_vrsel);
 
 	otg_put_transceiver(musb->xceiv);
-	usb_nop_xceiv_unregister();
+	usb_nop_xceiv_unregister(musb->id);
 	return 0;
 }
 
