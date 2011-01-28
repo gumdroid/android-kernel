audioscience/linux-omap3

ti816x: Add support for creating mapping for strongly-ordered memory for Ducati Unicache, MMU config space
This patch adds support for mappig strongly-ordered memory. The memory type is
called MT_STRONGLY_ORDERED and can be used for section mapping.

A static mapping of 1MB region of Media Controller L2 region is created. Drivers
can use standard 'ioremap' call to get the virtual mapping, for example, to get
Unicache/MMU config register space, do 'ioremap(0x55080000, 0x1000)'.

This is particularly added for Ducati MMU, Unicache space access as writes do
not work with 'Device' memory type, even when used with barriers.

_NOTE_1_: This is actually a 'HACK' patch and most likely be removed at some point.
_NOTE_2_: Need to dig h/w spec to get the actual reason write accessed with
	  device' type attribute are not working only for this region. Might as
	  well need some debugging.
_NOTE_3 : ***This creates section level mapping, that means at least 1MB Media
	  Controller L2 region staring from 0x55000000 is statically mapped and
	  includes not just Unicache/MMU space, but also L2 RAM, L2 MMU CFG etc.
	  This *might* result into side effects when accessing those other
	  modules.
	  ***

Signed-off-by: Hemant Pedanekar <hemantp@ti.com>

arch/arm/mach-omap2/io.c
@@ -246,6 +246,12 @@ static struct map_desc ti816x_io_desc[] __initdata = {
 		.length		= TI816X_GPMC_SIZE,
 		.type		= MT_DEVICE
 	},
+	{
+		.virtual	= TI816X_L2_MC_VIRT,
+		.pfn		= __phys_to_pfn(TI816X_L2_MC_PHYS),
+		.length		= TI816X_L2_MC_SIZE,
+		.type		= MT_STRONGLY_ORDERED
+	},
 };
 #endif
 

arch/arm/plat-omap/io.c
@@ -131,6 +131,8 @@ void __iomem *omap_ioremap(unsigned long p, size_t size, unsigned int type)
 					L4_SLOW_TI816X_VIRT);
 		if (BETWEEN(p, TI816X_GPMC_PHYS, OMAP44XX_GPMC_SIZE))
 			return XLATE(p, TI816X_GPMC_PHYS, TI816X_GPMC_VIRT);
+		if (BETWEEN(p, TI816X_L2_MC_PHYS, TI816X_L2_MC_SIZE))
+			return XLATE(p, TI816X_L2_MC_PHYS, TI816X_L2_MC_VIRT);
 	}
 #endif
 	return __arm_ioremap_caller(p, size, type, __builtin_return_address(0));

gumdroid/android-kernel

arch/arm/mach-omap2/io.c
@@ -237,6 +237,12 @@ static struct map_desc ti81xx_io_desc[] __initdata = {
 		.length		= L4_SLOW_TI81XX_SIZE,
 		.type		= MT_DEVICE
 	},
+	{
+		.virtual	= TI81XX_L2_MC_VIRT,
+		.pfn		= __phys_to_pfn(TI81XX_L2_MC_PHYS),
+		.length		= TI81XX_L2_MC_SIZE,
+		.type		= MT_STRONGLY_ORDERED
+	},
 };
 #endif
 

arch/arm/plat-omap/io.c
@@ -129,6 +129,8 @@ void __iomem *omap_ioremap(unsigned long p, size_t size, unsigned int type)
 		if (BETWEEN(p, L4_SLOW_TI81XX_PHYS, L4_SLOW_TI81XX_SIZE))
 			return XLATE(p, L4_SLOW_TI81XX_PHYS,
 					L4_SLOW_TI81XX_VIRT);
+		if (BETWEEN(p, TI81XX_L2_MC_PHYS, TI81XX_L2_MC_SIZE))
+			return XLATE(p, TI81XX_L2_MC_PHYS, TI81XX_L2_MC_VIRT);
 	}
 #endif
 	return __arm_ioremap_caller(p, size, type, __builtin_return_address(0));