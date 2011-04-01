0e86104944bec2d110fed0284d65aa9fc0a9c84b
Ajay Kumar Gupta <ajay.gupta@ti.com>
2011-04-01 13:04:13 +0530

    musb: add missing INDEX register save/restore
    This affects the offmode suspend/resume wherein musb doesn't work
    after resume.
    
    Signed-off-by: Amit Pundir <amitpundir@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2405,6 +2405,7 @@ void musb_save_context(struct musb *musb)
 				musb_read_rxhubport(musb_base, i);
 		}
 	}
+	musb_writeb(musb_base, MUSB_INDEX, musb->context.index);
 }
 EXPORT_SYMBOL_GPL(musb_save_context);
 
@@ -2476,6 +2477,7 @@ void musb_restore_context(struct musb *musb)
 				musb->context.index_regs[i].rxhubport);
 		}
 	}
+	musb_writeb(musb_base, MUSB_INDEX, musb->context.index);
 }
 EXPORT_SYMBOL_GPL(musb_restore_context);
 
