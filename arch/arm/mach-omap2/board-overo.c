balister/linux-omap-philip

omap: overo: Add support for Caspa camera module

arch/arm/mach-omap2/board-overo.c
@@ -73,6 +73,70 @@
 #define OVERO_SMSC911X2_CS     4
 #define OVERO_SMSC911X2_GPIO   65
 
+#if defined(CONFIG_VIDEO_OMAP3) || defined(CONFIG_VIDEO_OMAP3_MODULE)
+
+#include <media/mt9v032.h>
+#include "devices.h"
+#include "../../../drivers/media/video/omap3isp/isp.h"
+
+#define MT9V032_I2C_ADDR	0x5C
+#define MT9V032_I2C_BUS_NUM	3
+#define MT9V032_XCLK		ISP_XCLK_A
+
+static void mt9v032_set_clock(struct v4l2_subdev *subdev, int hz)
+{
+	struct isp_device *isp = v4l2_dev_to_isp_device(subdev->v4l2_dev);
+
+	printk("overo: setting xclk to %d hz\n", hz);
+	isp->platform_cb.set_xclk(isp, hz, MT9V032_XCLK);
+}
+
+static struct mt9v032_platform_data mt9v032_platform_data = {
+	.clk_pol		= 0,
+	.set_clock               = mt9v032_set_clock,
+};
+
+static struct i2c_board_info mt9v032_i2c_device = {
+	I2C_BOARD_INFO("mt9v032", MT9V032_I2C_ADDR),
+	.platform_data = &mt9v032_platform_data,
+};
+
+static struct isp_subdev_i2c_board_info mt9v032_subdevs[] = {
+	{
+		.board_info = &mt9v032_i2c_device,
+		.i2c_adapter_id = MT9V032_I2C_BUS_NUM,
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_v4l2_subdevs_group overo_camera_subdevs[] = {
+	{
+		.subdevs = mt9v032_subdevs,
+		.interface = ISP_INTERFACE_PARALLEL,
+		.bus = {
+				.parallel = {
+					.data_lane_shift = 0,
+					.clk_pol = 0,
+					.bridge = ISPCTRL_PAR_BRIDGE_DISABLE,
+				}
+		},
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_platform_data overo_isp_platform_data = {
+	.subdevs = overo_camera_subdevs,
+};
+
+static int __init overo_camera_init(void)
+{
+       return omap3_init_camera(&overo_isp_platform_data);
+}
+
+#else
+static inline void overo_camera_init(void) { return; }
+#endif
+
 #if defined(CONFIG_TOUCHSCREEN_ADS7846) || \
 	defined(CONFIG_TOUCHSCREEN_ADS7846_MODULE)
 
@@ -608,6 +672,7 @@ static void __init overo_init(void)
 	overo_init_led();
 	overo_init_keys();
 	overo_opp_init();
+	overo_camera_init();
 
 	/* Ensure SDRC pins are mux'd for self-refresh */
 	omap_mux_init_signal("sdrc_cke0", OMAP_PIN_OUTPUT);

gumdroid/android-kernel

arch/arm/mach-omap2/board-overo.c
@@ -74,6 +74,68 @@
 #define OVERO_SMSC911X2_CS     4
 #define OVERO_SMSC911X2_GPIO   65
 
+#if defined(CONFIG_VIDEO_OMAP3) || defined(CONFIG_VIDEO_OMAP3_MODULE)
+
+#include <media/mt9v032.h>
+#include "devices.h"
+#include "../../../drivers/media/video/omap3isp/isp.h"
+
+#define MT9V032_I2C_ADDR	0x5C
+#define MT9V032_I2C_BUS_NUM	3
+
+static void mt9v032_set_clock(struct v4l2_subdev *subdev, unsigned int rate)
+{
+	struct isp_device *isp = v4l2_dev_to_isp_device(subdev->v4l2_dev);
+
+	isp->platform_cb.set_xclk(isp, rate, ISP_XCLK_A);
+}
+
+static struct mt9v032_platform_data mt9v032_platform_data = {
+	.clk_pol = 0,
+	.set_clock = mt9v032_set_clock,
+};
+
+static struct i2c_board_info mt9v032_i2c_device = {
+	I2C_BOARD_INFO("mt9v032", MT9V032_I2C_ADDR),
+	.platform_data = &mt9v032_platform_data,
+};
+
+static struct isp_subdev_i2c_board_info mt9v032_subdevs[] = {
+	{
+		.board_info = &mt9v032_i2c_device,
+		.i2c_adapter_id = MT9V032_I2C_BUS_NUM,
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_v4l2_subdevs_group overo_camera_subdevs[] = {
+	{
+		.subdevs = mt9v032_subdevs,
+		.interface = ISP_INTERFACE_PARALLEL,
+		.bus = {
+				.parallel = {
+					.data_lane_shift = 0,
+					.clk_pol = 0,
+					.bridge = ISPCTRL_PAR_BRIDGE_DISABLE,
+				}
+		},
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_platform_data overo_isp_platform_data = {
+	.subdevs = overo_camera_subdevs,
+};
+
+static int __init overo_camera_init(void)
+{
+       return omap3_init_camera(&overo_isp_platform_data);
+}
+
+#else
+static inline void overo_camera_init(void) { return; }
+#endif
+
 #if defined(CONFIG_TOUCHSCREEN_ADS7846) || \
 	defined(CONFIG_TOUCHSCREEN_ADS7846_MODULE)
 
@@ -599,6 +661,7 @@ static void __init overo_init(void)
 	overo_init_led();
 	overo_init_keys();
 	overo_opp_init();
+	overo_camera_init();
 
 	/* Ensure SDRC pins are mux'd for self-refresh */
 	omap_mux_init_signal("sdrc_cke0", OMAP_PIN_OUTPUT);