JamesAng/lx-std

omap: beagle: Add support for 1GHz
Signed-off-by: Steve Sakoman <steve@sakoman.com>

arch/arm/mach-omap2/board-omap3beagle.c
@@ -859,12 +859,13 @@ static void __init beagle_opp_init(void)
 		/* Enable MPU 1GHz and lower opps */
 		dev = &mh->od->pdev.dev;
 		r = opp_enable(dev, 800000000);
-		/* TODO: MPU 1GHz needs SR and ABB */
+		r |= opp_enable(dev, 1000000000);
 
 		/* Enable IVA 800MHz and lower opps */
 		dev = &dh->od->pdev.dev;
 		r |= opp_enable(dev, 660000000);
-		/* TODO: DSP 800MHz needs SR and ABB */
+		r |= opp_enable(dev, 800000000);
+
 		if (r) {
 			pr_err("%s: failed to enable higher opp %d\n",
 				__func__, r);
@@ -874,8 +875,10 @@ static void __init beagle_opp_init(void)
 			 */
 			dev = &mh->od->pdev.dev;
 			opp_disable(dev, 800000000);
+			opp_disable(dev, 1000000000);
 			dev = &dh->od->pdev.dev;
 			opp_disable(dev, 660000000);
+			opp_disable(dev, 800000000);
 		}
 	}
 	return;

gumdroid/android-kernel

arch/arm/mach-omap2/board-omap3beagle.c
@@ -696,11 +696,12 @@ static void __init beagle_opp_init(void)
 		}
 		/* Enable MPU 1GHz and lower opps */
 		r = opp_enable(mpu_dev, 800000000);
-		/* TODO: MPU 1GHz needs SR and ABB */
+		r |= opp_enable(mpu_dev, 1000000000);
 
 		/* Enable IVA 800MHz and lower opps */
 		r |= opp_enable(iva_dev, 660000000);
-		/* TODO: DSP 800MHz needs SR and ABB */
+		r |= opp_enable(iva_dev, 800000000);
+
 		if (r) {
 			pr_err("%s: failed to enable higher opp %d\n",
 				__func__, r);
@@ -709,7 +710,9 @@ static void __init beagle_opp_init(void)
 			 * about the results
 			 */
 			opp_disable(mpu_dev, 800000000);
+			opp_disable(mpu_dev, 1000000000);
 			opp_disable(iva_dev, 660000000);
+			opp_disable(iva_dev, 800000000);
 		}
 	}
 	return;